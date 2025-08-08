c5d87eb8e247d3e62c5298abbc665a86af3c799b
shyamsundarb4 <ed20b061@smail.iitm.ac.in>
2025-08-08 12:07:15 +0530

    Initial commit with scribble clone

---
+++ scribble-main/README.md
@@ -0,0 +1,48 @@
+# Scribble
+
+A drawing app which allows multiple users to draw on the same canvas in real-time.
+
+<img width="1440" alt="Screenshot 2024-08-28 at 1 03 58 AM" src="https://github.com/user-attachments/assets/2828be9d-a6d0-4a44-9704-5057e548372d">
+
+## Features
+
+- color picker
+- customizable stroke color, stroke width, and dash gap
+- undo, save actions
+- toast notifications
+- light/dark theme
+
+## Tech Stack
+
+- Next.js
+- TailwindCSS
+- shadcn ui
+- React Hook Form
+- Zod
+- Zustand
+- Express.js
+- Socket.io
+
+## Getting Started
+
+Run this command in the root folder.
+
+```bash
+pnpm dev
+```
+
+or
+
+Change the directory into each folder and run.
+
+```bash
+cd server
+pnpm dev
+```
+
+```bash
+cd client
+pnpm dev
+```
+
+You can now play around with it.
---
+++ scribble-main/client/app/layout.tsx
@@ -0,0 +1,62 @@
+import type { Metadata, Viewport } from 'next'
+import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
+
+import { Toaster } from '@/components/ui/Sonner'
+import { Providers } from './providers'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+const TITLE = 'Scribble'
+const DESCRIPTION =
+  'Scribble is a drawing app which allows multiple users to draw on the same canvas in real-time.'
+
+export const metadata: Metadata = {
+  title: TITLE,
+  description: DESCRIPTION,
+  metadataBase: new URL('https://scribble.nainglinnkhant.com'),
+  twitter: {
+    card: 'summary_large_image',
+    title: TITLE,
+    description: DESCRIPTION,
+    images: ['/og'],
+    creator: '@nainglk',
+  },
+  openGraph: {
+    type: 'website',
+    title: TITLE,
+    description: DESCRIPTION,
+    images: ['/og'],
+    siteName: TITLE,
+    url: 'https://scribble.nainglinnkhant.com',
+    locale: 'en_US',
+  },
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon-16x16.png',
+    apple: '/apple-touch-icon.png',
+  },
+  manifest: '/site.webmanifest',
+}
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en' suppressHydrationWarning>
+      <body className={inter.className} suppressHydrationWarning>
+        <Providers>{children}</Providers>
+
+        <Toaster />
+
+        <Analytics />
+      </body>
+    </html>
+  )
+}
